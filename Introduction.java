a4d2999bf8018f47908ea562e4ca2985baac6c9f
nunoy6969 <53455637+nunoy6969@users.noreply.github.com>
2019-08-13 09:51:00 +0800

    Add files via upload

---
+++ Introduction.java
@@ -0,0 +1,44 @@
+
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Introduction{
+	
+	public static void main(String[]args){
+		Scanner objScanner1 = new Scanner(System.in);
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter your Name: ");
+		String x = scanner.nextLine();
+		
+		System.out.print("Enter your Address: ");
+		String y = scanner.nextLine();
+		
+		System.out.print("Enter your Course: ");
+		String z = scanner.nextLine();
+		
+		System.out.print("Enter your Age: ");
+		String e = scanner.nextLine();
+		
+		System.out.print("Enter your Contact Number: ");
+		String u = scanner.nextLine();
+	
+		System.out.println("Name: " + (x));
+		System.out.println("Address: " + (y));
+		System.out.println("Course: " + (z));
+		System.out.println("Age: " + (e));
+		System.out.println("Contact Number: " + (u));
+		
+		
+		
+		
+		
+		
+			}
+	
+	
+	
+	
+	
+}
